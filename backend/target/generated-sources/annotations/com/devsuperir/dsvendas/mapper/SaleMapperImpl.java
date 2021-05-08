175813b18747e12c633e7338e3624b5123329c30
Anderson Freitas <andersonfreitas21@gmail.com>
2021-05-07 21:27:50 -0300

    Routes

--- backend/target/generated-sources/annotations/com/devsuperir/dsvendas/mapper/SaleMapperImpl.java
+++ backend/target/generated-sources/annotations/com/devsuperir/dsvendas/mapper/SaleMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-05T21:23:13-0300",
+    date = "2021-05-07T10:12:01-0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.9.1 (Ubuntu)"
 )
 @Component
